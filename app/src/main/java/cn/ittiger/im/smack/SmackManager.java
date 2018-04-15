1a281bfba36c0254f5b386fe8050f5f49ecb3e88
Eric <liujunhong.z@gmail.com>
2018-04-15 10:11:25 +0800

    .

--- app/src/main/java/cn/ittiger/im/smack/SmackManager.java
+++ app/src/main/java/cn/ittiger/im/smack/SmackManager.java
@@ -45,7 +45,7 @@ public class SmackManager {
     /**
      * Xmpp服务器地址
      */
-    public static final String SERVER_IP = "121.42.13.79";
+    public static final String SERVER_IP = "47.97.118.48";
     /**
      * Xmpp 服务器端口
      */
@@ -53,7 +53,7 @@ public class SmackManager {
     /**
      * 服务器名称
      */
-    public static final String SERVER_NAME = "121.42.13.79";
+    public static final String SERVER_NAME = "47.97.118.48";
     /**
      *
      */
